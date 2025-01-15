2c24697496b5456edae48dc11d2636364eef2bf9
cheksumhov <matt@mattesmith.com>
2025-01-15 17:37:20 -0500

    Created player class and code to draw player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pygame
-from constants import *
+import pygame # import the pygame library
+from player import *
+from constants import * # import everything from constants.py
 
 def main():
     pygame.init()
@@ -7,16 +8,20 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+# Our main game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         screen.fill(color=000000)
+        player_obj = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        player_obj.draw(screen)
         pygame.display.flip()
 
         # limit FPS to 60
         dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
