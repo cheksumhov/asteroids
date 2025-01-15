a0e12c67ceb515231ab3180786cde2464adb9a04
cheksumhov <matt@mattesmith.com>
2025-01-15 18:10:37 -0500

    Added logic for rotating the player

--- main.py
+++ main.py
@@ -6,17 +6,21 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+    
+    player.rotate(dt)
 
 # Our main game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+    
+        player.update(dt)
         
         screen.fill(color=000000)
-        player_obj = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-        player_obj.draw(screen)
+        player.draw(screen)
         pygame.display.flip()
 
         # limit FPS to 60
