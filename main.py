41fd051c3b79bc1d6a0e5347656f886437b53d5e
cheksumhov <matt@mattesmith.com>
2025-01-15 19:45:01 -0500

    Added pygame groups to better organize code

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import pygame # import the pygame library
 from player import *
-from constants import * # import everything from constants.py
+from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
     
-    player.rotate(dt)
+   # player.rotate(dt)
 
 # Our main game loop
     while True:
@@ -17,10 +23,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
     
-        player.update(dt)
+        for object in updatable:
+            object.update(dt)
         
         screen.fill(color=000000)
-        player.draw(screen)
+
+        for object in drawable:
+            object.draw(screen)
+
         pygame.display.flip()
 
         # limit FPS to 60
